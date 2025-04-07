d685473f9ffa78605fac9aea51e8793d531a6fda
Nathanael Allen <nathanael.allen1@gmail.com>
2025-04-06 18:31:34 -0700

    UI Mockups

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {dirname, join} from "path";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+
+app.use(express.static(join(__dirname, "public", "static")));
+app.use(express.static(join(__dirname, "public", "assets")));
+app.use(express.static(join(__dirname, "public", "styles")));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, "index.html"))
+})
+
+app.listen(3000, () => {
+  console.log(new Date().toLocaleTimeString())
+  console.log("Server started\nhttp://localhost:3000");
+})
